b56e185cf8de2a2a08f3b5aa7c8d22ef9126e84d
juanesgh <jerodriguez@miuandes.cl>
2017-08-29 17:42:28 -0300

    agregue a tarea.c las notas de ahora

--- tarea.c
+++ tarea.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
+#include <unistd.h>
 
+
+int *sucursales;
+int *cuentas;
 struct sucursal{
 	int id;
 	struct cuenta *cts;
 	struct movimiento *mov;
 };
 struct cuenta{
-	int numero;
+	int numero;//numero no tiene la id de la sucursal
 	int balance;
 };
 struct movimiento{
+	//int id;
 	char[10] tipo;
 	int origen;//es la cuenta con el numero de sucursal
 	int destino;//idem^, pero puede ser vacio si tipo es retiro
 	int monto;
-	char[20] er-ex;//error-exito, error se puede considerar numero de cuenta invalido, para el random de transaccionesddd
+	char[20] er-ex;//error-exito, error se puede considerar numero de cuenta invalido, para el random de transaccionesd
 };
 	
 int main(int argv, char** argv){
@@ -30,11 +35,19 @@ int main(int argv, char** argv){
 	//y monto, para enviarle la instruccion a otro proceso. cada proceso se encarga de verificar si es posible la trasnsaccion, y debe enviar
 	//el mensaje de exito o falla al proceso que empezo la transaccion, ambos procesos guardan la info en sus respectivos structs
 	
-	//
+	//hacer funcion que decodifique la info dell buffer, y comprueba que exista saldo suficiente
+	//codifique la info y la solicitud de transferencia
+	//hacer la funcion que "decodifique" y reenvie la solicitud a las otras sucursales 
+	//hacer los pipes
+	//hacer la funcion que genere la transaccion
+	//hacer las funciones que requieren stdin
+	//hacer las funciones que escriben archivos
 	// if pid = 0, generar los structs correspondientes a sucursal, empezar a generar cuentas, luego transacciones. Cada vez que se 
 	//detecte comunicacion del pipe, el proceso que inicia la tarea weitea a que sus pipe reciba la info del proceso que necesita
 	//la info, para que ambos tengan la misma info si se realiza o no la operacion
 	
 	//usar una variable que guarde la cantidad de procesos creados, para qe el semaforo tenga claro, tambien se puede usa un
 	//semaforo binario para que solo se corra una tarea a la vez en la relacion pedir, puente, responder, puente, recibir
+	
+	
 }

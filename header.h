cc407c96377ee62be9d4c329692bb18c8e9ed170
Alfonso <ajzuniga1@miuandes.cl>
2017-09-03 21:42:33 -0300

    Varios cambios en el main

--- header.h
+++ header.h
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <math.h>
+#include <pthread.h>
 #include <sys/types.h>
 
 typedef struct
--- main.c
+++ main.c
@@ -48,6 +48,7 @@ int main(int argc, char** argv) {
   pid_t* sucursales = malloc(sizeof(sucursales));
   int total_sucursales = 0;
   int** pipes = malloc(sizeof(int[2]));
+  movimiento* movimientos;
   
   char readbuffer[80]; // buffer para lectura desde pipe
   
@@ -55,6 +56,8 @@ int main(int argc, char** argv) {
   printf("Bienvenido a Banco '%d'\n", bankId);
   
   int sucursal_creada = 0;
+
+  char* mensaje;
 
   while (true) {
   
@@ -112,70 +115,103 @@ int main(int argc, char** argv) {
       
       total_cuentas += atoi(c);
       
-      char *strnum = malloc(sizeof(char)*(3+sizeof(total_cuentas)+strlen(c)));
-      sprintf(strnum, "%d %s", total_cuentas, c);
-      printf("strnum: %s\n", strnum);
+      char *strnum = malloc(sizeof(char)*(3+sizeof(total_cuentas)+strlen(c)+1));
+      sprintf(strnum, "%d %s %d\0", total_cuentas, c, total_sucursales);
+      //printf("strnum: %s\n", strnum);
       pid_t sucid = fork();
 
       if (sucid > 0) {
       
-        printf("Sucursal creada con ID '%d'\n", sucid);
+        printf("Sucursal creada con ID '%d'\n", sucid%1000);
 
         // Enviando cantidad de cuentas a la sucursal a la sucursal
         sucursales = realloc(sucursales, sizeof(pid_t)*total_sucursales);
         sucursales[total_sucursales-1] = sucid;
         write(pipes[total_sucursales-1][1], strnum, (strlen(strnum)+1));
+        //int conf = read(pipes[total_sucursales-1][0], readbuffer, sizeof(readbuffer));
+        //printf("Soy la casa Matriz y leí el mensaje: '%s'\n", readbuffer);
+        //write(pipes[total_sucursales-1][1], "Hola", (strlen("Hola")));
+
+
 
         continue;
       }
       // Proceso de sucursal-----------------------------------------------
       else if (!sucid) {
-        movimiento* movimientos = malloc(sizeof(movimiento));
-        int total_cuentas;
-        cuenta** cuentas = malloc(sizeof(cuenta*));
-        int cantidad_cuentas = 1000;
-        int sucId = getpid() % 1000;
+        movimientos = malloc(sizeof(movimiento));   //La sucursal guarda todos los movimientos que se originaron de ella
+        int total_cuentas; //Tiene un int en el que guarda el total de cuentas de todo el banco
+        int numero_child;  //Corresponde al numero de child creado (primer hijo, segundo hijo...)
+        cuenta** cuentas = malloc(sizeof(cuenta*));   //Tiene un arreglo con todas sus cuentas
+        int cantidad_cuentas = 1000;    //Cantidad predeterminada de cuentas
+        int sucId = getpid() % 1000;    //Saca el PID del proceso sucursal
         printf("\n--------------\n");
         printf("Hola, soy la sucursal '%d'\n", sucId);
-        int bytes = read(bankPipe[0], readbuffer, sizeof(readbuffer));
-        printf("readbuffer: %s\n", readbuffer);
+        int bytes = read(pipes[numero_child][0], readbuffer, sizeof(readbuffer));
+        mensaje = readbuffer;
+        //printf("readbuffer: %s\n", mensaje);
         //Sacando la informacion del buffer
         int i;
-         
+
+        //Aca lee la cantidad de cuentas que existen en todo el banco         
         char *tc = malloc(sizeof(char));
         int counter = 0;
         
-        for (i=0;i<strlen(readbuffer);i++){
-          if(readbuffer[i]==' '){
+        for (i=0;i<strlen(mensaje);i++){
+          if(mensaje[i]==' '){
             counter = 0;
             break;
           } else {
             counter++;
             tc = realloc(tc, sizeof(char)*counter);
-            tc[counter-1] = readbuffer[i];
+            tc[counter-1] = mensaje[i];
           }
         }
         i++;
         
+        //Aca lee la cantidad de cuentas que debe crear esta sucursal
         char *cc = malloc(sizeof(char));
-        for (i; i<strlen(readbuffer);i++){
+        for (i; i<strlen(mensaje);i++){
+          if(mensaje[i]==' '){
+            counter = 0;
+            break;}
           counter++;
           cc = realloc(cc, sizeof(char)*counter);
-          cc[counter-1] = readbuffer[i];
+          cc[counter-1] = mensaje[i];
+        }
+
+        char* nc = malloc(sizeof(char));
+        for (i; i<strlen(mensaje);i++){
+          counter++;
+          nc = realloc(nc, sizeof(char)*counter);
+          nc[counter-1] = mensaje[i];
         }
         
-        printf("En sucursal... bID:%d tc:%s cc:%s\n", bankId, tc, cc);
+        //Despliega información sobre la sucursal y guarda valores en variables
+        //printf("En sucursal... bID:%d tc:%s cc:%s nc:%s\n", bankId, tc, cc, nc);
         
         cantidad_cuentas = atoi(cc);
         total_cuentas = atoi(tc);
-        printf("Tengo '%d' cuentas\n", cantidad_cuentas);
+        numero_child = atoi(nc)-1;
+        printf("Tengo '%d' cuentas\n\n", cantidad_cuentas);
+        //write(pipes[numero_child][1], "Hola", (strlen("Hola")+1));
 
-        crear_cuentas(cantidad_cuentas, bankId, sucid, cuentas);
+        //Procede a crear las cuentas
+        crear_cuentas(cantidad_cuentas, bankId, sucId, cuentas);
         
         while (true) {
-          printf("En el while\n");
-          bytes = read(bankPipe[0], readbuffer, sizeof(readbuffer));
-          printf("Soy la sucursal '%d' y leí el mensaje: '%s'\n", sucId, readbuffer);
+          char* mensaje = codificar_desde_sucursal('d', cuentas[random_number(0,cantidad_cuentas)], random_number(0, total_cuentas), random_number(1, 5000000));
+          //write(pipes[numero_child][1], mensaje, strlen(mensaje));
+          //printf("En el while\n");
+          bytes = read(pipes[numero_child][0], readbuffer, sizeof(readbuffer));
+          mensaje = readbuffer;
+          printf("Soy la sucursal '%d' y leí el mensaje: '%s'\n", sucId, mensaje);
+          /*
+          pthread_t p1, p2;
+          int rc = 0;
+
+          rc = pthread_create(&p1, NULL, enviar_como_emisor, info);
+          rc = pthread_create(&p2, NULL, recibir_mensaje, info);
+          */
           
           if (!strncmp("quit", readbuffer, strlen("quit"))){
             free(movimientos);
@@ -232,8 +268,11 @@ int main(int argc, char** argv) {
     }
     else if (!strncmp("dump", commandBuf, strlen("dump"))) {
       int num = dump_input("dump_accs", sucursales, commandBuf);
-      //Creación archivo csv de movimientos en una sucursal
-      printf("Archivo creado\n");
+      if (getpid() == 0)
+      {
+        dump_csv(movimientos, getpid()%1000);
+        printf("Archivo creado\n");
+      }
     }
     else {
       fprintf(stderr, "Comando no reconocido.\n");
